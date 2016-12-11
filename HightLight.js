7def6eaf3bb2c2746e69d61514fbd18a3b9c3e63
zlargon <zlargon@icloud.com>
2016-12-11 19:37:49 +0800

    make HightLight.js support both Node and browser platform

--- HightLight.js
+++ HightLight.js
@@ -1,58 +1,70 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var hljs = require('highlight.js');
+(function (window) {
 
-var HighLight = React.createClass({
-  propTypes: {
-    lang: React.PropTypes.string.isRequired,
-    value: React.PropTypes.string.isRequired
-  },
+  function getHighLight(React, ReactDOM, hljs) {
+    return React.createClass({
+      propTypes: {
+        lang: React.PropTypes.string.isRequired,
+        value: React.PropTypes.string.isRequired
+      },
 
-  componentDidMount: function () {
-    // this will only be called once after first time render
-    this.updateCodeBlockDOM();
-  },
+      componentDidMount: function () {
+        // this will only be called once after first time render
+        this.updateCodeBlockDOM();
+      },
 
-  componentDidUpdate: function () {
-    // whenever component is updated
-    this.updateCodeBlockDOM();
-  },
+      componentDidUpdate: function () {
+        // whenever component is updated
+        this.updateCodeBlockDOM();
+      },
 
-  render: function () {
-    /*
-     * <pre inherit_all_the_props_from_parent >
-     *  <code ref='code' className={'hljs ' + this.props.lang}>
-     *    to_be_rendered_by_highlight.js
-     *  </code>
-     * </pre>
-     *
-     */
+      render: function () {
+        /*
+         * <pre inherit_all_the_props_from_parent >
+         *  <code ref='code' className={'hljs ' + this.props.lang}>
+         *    to_be_rendered_by_highlight.js
+         *  </code>
+         * </pre>
+         *
+         */
 
-    var props = Object.assign({}, this.props);
-    delete props.lang;
-    delete props.value;
+        var props = Object.assign({}, this.props);
+        delete props.lang;
+        delete props.value;
 
-    return (
-      React.createElement('pre', props,
-        React.createElement('code', {
-          ref: 'code',
-          className: 'hljs ' + this.props.lang
-        })
-      )
-    );
-  },
+        return (
+          React.createElement('pre', props,
+            React.createElement('code', {
+              ref: 'code',
+              className: 'hljs ' + this.props.lang
+            })
+          )
+        );
+      },
 
-  updateCodeBlockDOM: function () {
-    // update real DOM element after component render
-    var ele = ReactDOM.findDOMNode(this.refs.code);
+      updateCodeBlockDOM: function () {
+        // update real DOM element after component render
+        var ele = ReactDOM.findDOMNode(this.refs.code);
 
-    try {
-      ele.innerHTML = hljs.highlight(this.props.lang, this.props.value, true).value;
-    } catch (e) {
-      console.warn(e);
-      ele.innerHTML = this.props.value; // remove syntax highlight
-    }
+        try {
+          ele.innerHTML = hljs.highlight(this.props.lang, this.props.value, true).value;
+        } catch (e) {
+          console.warn(e);
+          ele.innerHTML = this.props.value; // remove syntax highlight
+        }
+      }
+    });
   }
-});
 
-module.exports = HighLight;
+  if (typeof window === 'undefined') {
+    // Node: export as module
+    module.exports = getHighLight(
+      require('react'),
+      require('react-dom'),
+      require('highlight.js')
+    );
+  } else {
+    // Browser: export as global variable
+    window.HighLight = window.HighLight || getHighLight(React, ReactDOM, hljs);
+  }
+
+})(this);
